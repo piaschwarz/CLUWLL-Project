231a03cf3f14bc69396bdda7c93ceac779a97c1d
piaschwarz <38730179+piaschwarz@users.noreply.github.com>
2020-04-05 21:18:34 +0200

    Add files via upload

--- code_preprocessing/retrieve_context_sents.py
+++ code_preprocessing/retrieve_context_sents.py
@@ -1,31 +1,27 @@
 """
 Script to retrieve useful context sentences. A sentence is considered of good quality if
-it contains at least two content words (low frequency words) additionally to the lemma.
-Then they are ranked, where sentences with the least frequent content words are ranked before
-sentences with more frequent content words.
+it contains at least two content words (low frequency words) additionally to the target word. Words with the
+following POS tags are considered content word: Nouns, Verbs, Adjectives and Adverbs
 
-1. Collect only the sentences that contain enough content words. A word is considered a low freq. word
-   if its normalized freq. count is below a certain threshold (tbd)
-   The frequency of a word needs to be extracted from a corpus like OpenSubtitles.org - a csv/tsv file with
-   the following format needs to be present:
-    ----------------------------------
-    lemma1  normalized_frequency_count
-    lemma2  normalized_frequency_count
-    ...
-    ----------------------------------
+These "high quality" context sentences are then ranked, where sentences with many content words are
+ranked before sentences with few content words. If several sentences have the same amount of content words,
+the average frequency of all content words of a sentence is computed and sentences with a lower average
+frequency get ranked first.
 
-2. rank according to the average frequency of all content words in a sentence.
+1. A list of content words and their absolute frequency is retrieved from the Subtlex-it corpus file
+    from http://crr.ugent.be/subtlex-it/
+    Only words with the dominant Lemma POS = NOM, VER, ADJ or ADV are considered content words
+    Additionally their absolute frequency count has to be below a threshold of 204981 which was set after
+    manually checking the subtlex-it frequency list, it might be subject to change
+
+2. The average frequency of all content words in a sentence is computed the following way:
    Example:
     lemma = mangiare
     [Sent_deu = "Wir werden heute Abend viel essen, daher hoffe ich, dass du nicht auf Diät bist"]
     Sent_ita = "Mangeremo molto stasera e quindi spero che tu non sia a dieta."
-    Frequencies:  0.04             0.05          0.003                  0.00008
-                (assuming only the freq. counts of these words are below the threshold mentioned above)
-                --> Average_freq = (0.04 + 0.05 + 0.003 + 0.00008) / 3
-
-The following files are used:
-- context_sentences.csv (lists all Italian lemmas and a list of sentence IDs that contain this word (non-lemmatized) --> AVAILABLE
-- frequencies.csv (lists all words (lemmatized) and their normalized frequency count from a huge corpus like OpenSubtitles --> TO DO
+    Frequencies:  29654          18122     5235  35668                  4258
+                (considering only the words whose freq. counts are below the threshold of 204981)
+                --> Average_freq = (29654 + 18122 + 5235 + 35668 + 4258) / 5
 
 Author: Pia Schwarz
 """
@@ -37,7 +33,7 @@ from preprocess_tatoeba import dict_to_csv
 
 def get_subtlex_content_word_freqs(subtlex_it_file):
     """ Retrieves the absolute frequencies of the dominant lemma form of a type from the file subtlex-it.csv
-        Only considers content words, meaning verbs, adjectives and nouns
+        Only considers content words, meaning nouns, verbs, adjectives and adverbs
         :arg
             subtlex_it_file: path to subtlex-it.csv from http://crr.ugent.be/subtlex-it/
         :returns
@@ -51,7 +47,7 @@ def get_subtlex_content_word_freqs(subtlex_it_file):
             dom_pos = row[4]
             dom_lemma = row[5]
             dom_lemma_freq = row[6]
-            if dom_pos == "VER" or "ADJ" or "NOM":
+            if dom_pos == "VER" or "ADJ" or "NOM" or "ADV":
                 freqs[dom_lemma] = dom_lemma_freq
 
     return freqs
@@ -79,62 +75,73 @@ def load_file_as_dict(file, delimiter='\t'):
 
 if __name__ == '__main__':
 
+    ####################################################################################################################
+    # USE THIS BLOCK TO GET QUALITY CONTEXT SENTENCES FOR THE TARGET WORDS OF LEVEL B1 AND B2
 
+    # retrieve absolute content word frequencies from the subtlex-it corpus and store them into dict:
+    # key = lemma, value = frequency
     lemma_freqs = get_subtlex_content_word_freqs("/home/pia/Schreibtisch/CLUWLL-Project/subtlex-it.csv")
-    lemma_freqs = {k: int(v) for k, v in lemma_freqs.items()}
+    lemma_freqs = {k: int(v) for k, v in lemma_freqs.items()}  # type cast dict keys to integers
 
+    # load the sentence lists for words from B1 and B2: key = lemma, value = sent. ID list containing this lemma
     context_sents_lists = load_file_as_dict("/home/pia/Schreibtisch/CLUWLL-Project/preprocessing_tatoeba/context_sentences_B1B2.csv")
-    context_sents_lists = {k: ast.literal_eval(v) for k, v in context_sents_lists.items()}
+    context_sents_lists = {k: ast.literal_eval(v) for k, v in context_sents_lists.items()}  # make sure that dict values are evaluated as lists
 
+    # load the lemmatized Italian sentences (called 'selected' becaus they have German translations available):
+    # key = ID, value = lemmatized Italian sentence
     selected_sentences_lemma_ita = load_file_as_dict("/home/pia/Schreibtisch/CLUWLL-Project/preprocessing_tatoeba/selected_sentences_lemma_ita.csv")
-    selected_sentences_lemma_ita = {int(k): v for k, v in selected_sentences_lemma_ita.items()}
+    selected_sentences_lemma_ita = {int(k): v for k, v in selected_sentences_lemma_ita.items()}  # type cast dict keys to integers
 
-    """
-    lemma_context_useful = defaultdict(list)  # creates a dict with a list as the value
+    # create a dict with key = lemma and value = list that will contain the IDs of the sentences that are considered
+    # quality sentences (have enough content words: target word + two content words)
+    lemma_context_useful = defaultdict(list)
 
     for key_lemma in context_sents_lists.keys():
+
         print("\n--------------------------- LEMMA = " + key_lemma + "----------------------------------------------------------------------------------------")
         #print("lemma: ", key_lemma, " - IDs before: ", context_sents_lists[key_lemma])
+
         rankings = []  # collects the tuples (ID, average_content_word_frequency)
 
-        for id in context_sents_lists[key_lemma]:
-            lemmatized_sent = selected_sentences_lemma_ita[id]
-            content_word_counter = 0  # counts the amount of low frequency
-            # content words in the lemmatized sentence
-            content_word_freq = 0     # sums up the absolute frequency of all low frequency content words in the lemmatized sentence
+        for id in context_sents_lists[key_lemma]:  # iterate through all sent. IDs assigned to the lemma
+            lemmatized_sent = selected_sentences_lemma_ita[id]  # get single lemmatized sentence
+            content_word_counter = 0  # counts the amount of low frequency content words in the lemmatized sentence
+            content_word_freq = 0  # sums up the absolute frequency of all low frequency content words in the lemmatized sentence
 
             for word in lemmatized_sent.split():
                 if word != key_lemma:  # ignore the lemma which is the key of the ID dict
                     # check if lemma is in the frequency dict and has a frequency low enough to count as a "low frequency content word"
-                    if lemma_freqs.get(word) != None and lemma_freqs[word] <= 204981:  # maybe it is necessary to change this threshold
+                    # the threshold of 204981 was set after manually checking the subtlex-it frequency list, it might be changed
+                    if lemma_freqs.get(word) != None and lemma_freqs[word] <= 204981:  # threshold might be changed
                         content_word_counter += 1
                         content_word_freq += lemma_freqs[word]
 
-            if content_word_counter >= 2:
-                print("sentence: ", lemmatized_sent, " -- content word amount: ", content_word_counter)
-            # calculate average freq and add tuple (ID, average_freq) to ranking list
-            if content_word_counter >= 2:
-                average_freq = int(content_word_freq/content_word_counter)
-                tup = (id, average_freq)
+            if content_word_counter >= 2:  # only consider sentences with enough content words
+                average_freq = int(content_word_freq / content_word_counter)
+                tup = (id, average_freq, content_word_counter)
                 rankings.append(tup)
 
-        # sort the ranking list by the second tuple item: average_freq
-        rankings.sort(key=lambda x: x[1])
-        # make a pure ID list out of the tuple list
-        sorted_list = []
+            # sort the rankings so we get the sentences with the most content words at the beginning of the list and if
+            # there are sentences with the same amount of content words, sort them by their average content word frequency:
+            rankings.sort(key=lambda x: x[1])  # first sort by the second tuple item: average content word frequency ...
+            rankings.sort(key=lambda x: x[2], reverse=True)  # ... then reverse sort by the third tuple item: amount of content word
 
-        for t in rankings:
-            sorted_list.append(t[0])
+            sorted_list = []  # make a pure ID list out of the list containing both, ID and average frequency
+            for t in rankings:
+                sorted_list.append(t[0])  # t[0] selects only the ID from the tuples (ID, average_freq, content_word_counter)
 
-        # add the sorted list to dict lemma_context_useful with key=lemma, value=sorted list
-        lemma_context_useful[key_lemma] = sorted_list
+            # add the sorted list to dict lemma_context_useful with: key = lemma, value = sorted list
+            lemma_context_useful[key_lemma] = sorted_list
+
         #print("lemma: ", key_lemma, " - IDs after: ", lemma_context_useful[key_lemma])
 
     #dict_to_csv(lemma_context_useful, "/home/pia/Schreibtisch/CLUWLL-Project/preprocessing_tatoeba/context_sentences_B1B2_quality.csv")
+    ####################################################################################################################
 
-
-
-    # converts dictionary to csv file - USE TO TEST FOR LEMMAS THAT HAVE FEWER THAN X CONTEXT SENTENCES
+    """
+    ####################################################################################################################
+    # USE THIS BLOCK TO SELECT FOR LEMMAS THAT HAVE FEWER THAN X CONTEXT SENTENCES - converts dictionary to csv file
+    
     out_file = "/home/pia/Schreibtisch/CLUWLL-Project/preprocessing_tatoeba/B1B2_lemmas_with_less_than_3_context_sentences_quality.csv"
     dictionary = lemma_context_useful
     with open(out_file, 'wt') as fo:
@@ -143,9 +150,14 @@ if __name__ == '__main__':
             if len(sent_list) < 3:
                 csv_writer.writerow([lemma, sent_list])
     print("finished writing to file: ", out_file)
+    ####################################################################################################################
     """
 
 
+
+    ####################################################################################################################
+    # USE THIS BLOCK TO PRINT THE QUALITY CONTEXT SENTENCES IN ITALIAN (LEMMATIZED & NON-LEMMATIZED) AND THEIR GERMAN TRANSLATIONS TO A FILE
+    
     selected_sentences_ita = load_file_as_dict("/home/pia/Schreibtisch/CLUWLL-Project/preprocessing_tatoeba/selected_sentences_ita.csv")
     selected_sentences_ita = {int(k): v for k, v in selected_sentences_ita.items()}
 
@@ -173,6 +185,7 @@ if __name__ == '__main__':
                 f.write("[GER-N]: " + s_g + "\n")
                 f.write("---------------------------------------------------------------------------------------------------------------\n")
             f.writelines("\n\n")
+    ####################################################################################################################
 
 
 
